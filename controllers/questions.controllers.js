0fb5f95a461995c4bdd7dcbd834cc3530369a324
Aman Khare <khareaman754@gmail.com>
2025-05-31 13:36:32 +0530

    feat: again added mail logic

--- controllers/questions.controllers.js
+++ controllers/questions.controllers.js
@@ -2,46 +2,46 @@ const nodeMailer = require("nodemailer");
 const SingleUser = require("../models/singleUserSchema.model");
 require("dotenv").config({ path: ".env" });
 
-// // Configure mail
-// const sendMail = async (userData) => {
-//   // Creating email transporter,
-//   // SMTP (Simple Mail Transfer Protocol)
-//   const transporter = nodeMailer.createTransport({
-//     service: "gmail",
-//     auth: {
-//       user: "aman.khare@messold.com",
-//       pass: process.env.AMAN_MESSOLD_PASS,
-//     },
-//   });
+// Configure mail
+const sendMail = async (userData) => {
+  // Creating email transporter,
+  // SMTP (Simple Mail Transfer Protocol)
+  const transporter = nodeMailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: "aman.khare@messold.com",
+      pass: process.env.AMAN_MESSOLD_PASS,
+    },
+  });
 
-//   // Configure email content
-//   const mailOptions = {
-//     from: "aman.khare@messold.com",
-//     to: `${userData?.questionsAnswers?.[1]?.answer}`,
-//     subject: "Your filled details",
-//     html: `<ul>
-//     <h2 style="padding: 0.5rem 0;">Your Messold Form Details</h2>
-//     ${userData?.questionsAnswers
-//       ?.map((details) => {
-//         return `
-//       <li style="list-style: none; margin-bottom: 10px;">
-//           <h3>
-//             <span>${details?.title}</span>: <strong>${details?.answer}</strong>
-//           </h3>
-//         </li>`;
-//       })
-//       .join("")}
-//     </ul>`,
-//   };
+  // Configure email content
+  const mailOptions = {
+    from: "aman.khare@messold.com",
+    to: `${userData?.questionsAnswers?.[1]?.answer}`,
+    subject: "Your filled details",
+    html: `<ul>
+    <h2 style="padding: 0.5rem 0;">Your Messold Form Details</h2>
+    ${userData?.questionsAnswers
+      ?.map((details) => {
+        return ` 
+      <li style="list-style: none; margin-bottom: 10px;">
+          <h3>
+            <span>${details?.title}</span>: <strong>${details?.answer}</strong>
+          </h3>
+        </li>`;
+      })
+      .join("")}
+    </ul>`,
+  };
 
-//   // Send Email
-//   try {
-//     await transporter.sendMail(mailOptions);
-//     console.log("Email sent successfully!");
-//   } catch (error) {
-//     console.log("Error while sending email: ", error);
-//   }
-// };
+  // Send Email
+  try {
+    await transporter.sendMail(mailOptions);
+    console.log("Email sent successfully!");
+  } catch (error) {
+    console.log("Error while sending email: ", error);
+  }
+};
 
 const addQuestion = async (req, res) => {
   const userData = req.body;
